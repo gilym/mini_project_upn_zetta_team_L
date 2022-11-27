82a878941f5ffd49f2d9bcffa9c00934532cbee6
Gilang Yoenal Marinta <gilym46@gmail.com>
2022-11-28 01:53:02 +0900

    commit

--- mini_project_upn_zetta_team_L/src/app/home/home-routing.module.ts
+++ mini_project_upn_zetta_team_L/src/app/home/home-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path :'',
+    pathMatch:'full',
+    component :HomeComponent
+  
+  },
+  {
+    path: '**' ,
+    pathMatch:'full',
+   redirectTo : ''
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- mini_project_upn_zetta_team_L/src/app/home/home.module.ts
+++ mini_project_upn_zetta_team_L/src/app/home/home.module.ts
@@ -5,13 +5,34 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 
 
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list'
+import { FormsModule } from '@angular/forms';
+
+import {ReactiveFormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+     MatSelectModule,
+    MatRadioModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatDividerModule,
   ]
 })
 export class HomeModule { }
