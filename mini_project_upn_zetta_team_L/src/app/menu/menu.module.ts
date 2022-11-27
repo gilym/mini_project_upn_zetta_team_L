bc183b6c163baafc3417f12fea9c66060e5d1942
M.Dzaki <dzakibs@gmail.com>
2022-11-28 01:38:40 +0700

    generate component

--- mini_project_upn_zetta_team_L/src/app/menu/menu.module.ts
+++ mini_project_upn_zetta_team_L/src/app/menu/menu.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MenuRoutingModule } from './menu-routing.module';
+import { MenuComponent } from './menu.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MenuComponent
+  ],
   imports: [
     CommonModule,
     MenuRoutingModule
