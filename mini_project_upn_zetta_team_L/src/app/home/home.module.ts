2eb62d7a1ca0b158899312761af269a02da45463
Gilang Yoenal Marinta <gilym46@gmail.com>
2022-12-08 02:53:12 +0900

    ui home landing page

--- mini_project_upn_zetta_team_L/src/app/home/home.module.ts
+++ mini_project_upn_zetta_team_L/src/app/home/home.module.ts
@@ -3,6 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { AngularMaterial } from '../shared/angular.material';
+
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list'
+import { FormsModule } from '@angular/forms';
+
+import {ReactiveFormsModule} from '@angular/forms';
+
 
 
 @NgModule({
@@ -11,7 +27,21 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatGridListModule,
+    AngularMaterial,
+ 
+    MatListModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
--- mini_project_upn_zetta_team_L/src/app/home/home/home.component.ts
+++ mini_project_upn_zetta_team_L/src/app/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+export interface item {menu : string , desc : string, harga : number , image : any}
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  items: item[] = [
+    {menu: 'Sushi1', desc: 'ini dia sushi 1' , harga : 50000 ,image : "https://asset-a.grid.id//crop/0x0:0x0/700x465/photo/2022/01/31/sushi-gaacfb5515_640-1jpg-20220131023134.jpg"},
+    {menu: 'Sushi2', desc: 'ini dia sushi 2' , harga : 10000 ,image : "https://static.honestdocs.id/system/blog_articles/main_hero_images/000/002/990/original/Ikan_Mentah_Pada_Hidangan_Sushi__Aman_Untuk_di_Konsumsi.jpg"},
+    {menu: 'Sushi3', desc: 'ini dia sushi 3' , harga : 100000 ,image : "https://cdn.bobobox.co.id/blog/wp-content/uploads/2020/08/14132844/All-Variant-Sushi.jpg"} ,
+   
+  ];
 
   constructor() { }
 
